31b108fca7320c283effcdf2cea3570aa1ea37c6
Guillaume Gauvrit <guillaume@gauvr.it>
2025-12-07 12:01:06 +0100

    Tests the prometheus implementation

--- src/messagebus/infrastructure/observability/metrics.py
+++ src/messagebus/infrastructure/observability/metrics.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Any, ClassVar
 
 from prometheus_client import REGISTRY, CollectorRegistry, Counter, Gauge
 
@@ -16,38 +17,52 @@ class AbstractMetricsStore(abc.ABC):
     def inc_transaction_closed_count(self, status: TransactionStatus) -> None: ...
 
 
-class MetricsStore(AbstractMetricsStore):
-    _instance = None
-    _initialized = False
+class Singleton(abc.ABCMeta):
+    """
+    Ensure the REGISTRY will not raises a duplicate timeseries by reusing it.
 
-    def __new__(cls, registry: CollectorRegistry | None = REGISTRY) -> "MetricsStore":
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
+    Since we as an Async and a Sync version we got two instances of the MetricStore,
+    and to avoid gen unasync code, this singleton ensure we have one class.
+    The registry in parameter ensure the class behave properly by registry,
+    and might create memory leaks if misused.
+    """
 
-    def __init__(self, registry: CollectorRegistry | None = REGISTRY) -> None:
-        if self._initialized:
-            return
-        self._initialized = True
+    _instances: ClassVar[dict[tuple[type, int], Any]] = {}
+
+    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
+        registry = kwargs.get("registry") or REGISTRY
+
+        key = (cls, id(registry))
+        if key not in cls._instances:
+            cls._instances[key] = super().__call__(*args, **kwargs)
+        return cls._instances[key]
+
+
+class MetricsStore(AbstractMetricsStore, metaclass=Singleton):
+    def __init__(self, *, registry: CollectorRegistry = REGISTRY) -> None:
         self.transactions_started_total = Counter(
             name="messagebus_transactions_started_total",
             documentation="Total number of unit-of-work transactions that have been started.",
+            registry=registry,
         )
 
         self.transactions_in_progress = Gauge(
             name="messagebus_transactions_in_progress",
             documentation="Current number of unit-of-work transactions in progress.",
+            registry=registry,
         )
 
         self.transactions_failed_total = Counter(
             name="messagebus_transactions_failed_total",
             documentation="Total number of unit-of-work transactions rolled back due to an exception.",
+            registry=registry,
         )
 
         self.transactions_closed_total = Counter(
             name="messagebus_transactions_closed_total",
             documentation="Total number of unit-of-work transactions that have been committed or rolled back.",
             labelnames=["status"],
+            registry=registry,
         )
 
     def inc_beginned_transaction_count(self) -> None:
---
+++ tests/infrastructure/test_observability.py
@@ -0,0 +1,94 @@
+from collections.abc import Iterator
+
+import pytest
+from prometheus_client import CollectorRegistry
+
+from messagebus import TransactionStatus
+from messagebus.infrastructure.observability.metrics import MetricsStore, Singleton
+
+
+@pytest.fixture()
+def registry() -> CollectorRegistry:
+    return CollectorRegistry()
+
+
+@pytest.fixture()
+def metrics(registry: CollectorRegistry) -> Iterator[MetricsStore]:
+    yield MetricsStore(registry=registry)
+    del Singleton._instances[(MetricsStore, id(registry))]
+
+
+def test_prometheus_transaction_commit(
+    metrics: MetricsStore, registry: CollectorRegistry
+):
+    metrics.inc_beginned_transaction_count()
+    assert registry.get_sample_value("messagebus_transactions_started_total") == 1
+    assert registry.get_sample_value("messagebus_transactions_in_progress") == 1
+    assert registry.get_sample_value("messagebus_transactions_failed_total") == 0
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "committed"}
+        )
+        is None
+    )
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "rolledback"}
+        )
+        is None
+    )
+
+    metrics.inc_transaction_closed_count(TransactionStatus.committed)
+    assert registry.get_sample_value("messagebus_transactions_started_total") == 1
+    assert registry.get_sample_value("messagebus_transactions_in_progress") == 0
+    assert registry.get_sample_value("messagebus_transactions_failed_total") == 0
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "committed"}
+        )
+        == 1
+    )
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "rolledback"}
+        )
+        is None
+    )
+
+
+def test_prometheus_transaction_rollback(
+    metrics: MetricsStore, registry: CollectorRegistry
+):
+    metrics.inc_beginned_transaction_count()
+    assert registry.get_sample_value("messagebus_transactions_started_total") == 1
+    assert registry.get_sample_value("messagebus_transactions_in_progress") == 1
+    assert registry.get_sample_value("messagebus_transactions_failed_total") == 0
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "committed"}
+        )
+        is None
+    )
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "rolledback"}
+        )
+        is None
+    )
+
+    metrics.inc_transaction_closed_count(TransactionStatus.rolledback)
+    assert registry.get_sample_value("messagebus_transactions_started_total") == 1
+    assert registry.get_sample_value("messagebus_transactions_in_progress") == 0
+    assert registry.get_sample_value("messagebus_transactions_failed_total") == 0
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "committed"}
+        )
+        is None
+    )
+    assert (
+        registry.get_sample_value(
+            "messagebus_transactions_closed_total", labels={"status": "rolledback"}
+        )
+        == 1
+    )
